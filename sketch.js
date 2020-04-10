70c7a35c98bc73bf99e0849c6d04b76b361892c5
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-04-10 11:48:56 -0400

    fixed motion + optimizations

--- sketch.js
+++ sketch.js
@@ -1,10 +1,17 @@
+p5.disableFriendlyErrors = true; // disables FES
+
+function distSquared(x1, y1, x2, y2) {
+    let dx = x2 - x1;
+    let dy = y2 - y1;
+    return dx * dx + dy * dy;
+}
 let angle  = 0;
 let w =35;
 let ma;
 function setup() {
-  createCanvas(700, 700, WEBGL);
+  createCanvas(650, 650, WEBGL);
   ma = atan(1/sqrt(2));
-  maxD = dist(0,0,200,200);
+  maxD = distSquared(0,0,200,200);
 }
 function draw() {
   let locX = height / 2;
@@ -22,11 +29,11 @@ function draw() {
   for(let z = w; z < height-w; z+=w){
     for(let x = w; x < width-w; x+=w){
       push()
-      let d = dist(x,z,width/2,height/2);
-      let offset = map(d,0,maxD, PI,-PI);
+      let d = distSquared(x,z,width/2,height/2);
+      let offset = map(d,0,maxD, PI,-QUARTER_PI);
       translate(x-width/2,0,z-height/2);
-      let h = floor(map(sin(angle+offset),-1,1, 140, 400));
-      fill(250, 0, 0);
+      let h = floor(map(Math.sin(angle+offset),-1,1, 140, 400));
+      normalMaterial();
       box(w, h, w);
       //rect(x,0,10,h); 
       
